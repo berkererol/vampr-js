bfbca2d6d3c42f38428d926555a1962fc26d4d6a
berkererol <berkererol@gmail.com>
2019-09-08 16:30:50 +0000

    Final code with strecth

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampiresFromOriginal = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = this.creator;
+      numberOfVampiresFromOriginal++;
+    }
+    return numberOfVampiresFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,6 +43,12 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this.isMoreSeniorThan(vampire)) {
+      return this;
+    } else {
+      return vampire;
+    }
+
 
   }
 }
